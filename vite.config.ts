5e8554f864a9ca97fa964f17c1bb070b51ad1a66
Kamron <acomputerguru01@outlook.com>
2025-05-28 13:18:28 +0500

    fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // agar React ishlatilsa
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
 })
